c3b6e172614eb53468fb8cec50000b99b3681c2b
sniperrifle2004 <sniperrifle2004+github@gmail.com>
2017-12-18 12:01:22 +0100

    Improve layout for imports

--- src/Language/Haskell/Brittany/Internal/Layouters/Import.hs
+++ src/Language/Haskell/Brittany/Internal/Layouters/Import.hs
@@ -7,17 +7,14 @@ import           Language.Haskell.Brittany.Internal.LayouterBasics
 import           Language.Haskell.Brittany.Internal.Layouters.IE
 import           Language.Haskell.Brittany.Internal.Config.Types
 
-import           RdrName (RdrName(..))
-import           GHC     ( unLoc
-                         , runGhc
-                         , GenLocated(L)
-                         , moduleNameString
-                         , AnnKeywordId(..)
-                         , Located
-                         )
+import           RdrName                                  ( RdrName(..) )
+import           GHC                                      ( unLoc
+                                                          , GenLocated(L)
+                                                          , moduleNameString
+                                                          , Located
+                                                          )
 import           HsSyn
 import           Name
-import           HsImpExp
 import           FieldLabel
 import qualified FastString
 import           BasicTypes
@@ -28,12 +25,11 @@ import           Language.Haskell.Brittany.Internal.Utils
 
 #if MIN_VERSION_ghc(8,2,0)
 prepPkg :: SourceText -> String
-prepPkg rawN    =
-  case rawN of
-    SourceText n -> n
-    -- This would be odd to encounter and the
-    -- result will most certainly be wrong
-    NoSourceText -> ""
+prepPkg rawN = case rawN of
+  SourceText n -> n
+  -- This would be odd to encounter and the
+  -- result will most certainly be wrong
+  NoSourceText -> ""
 #else
 prepPkg :: String -> String
 prepPkg = id
@@ -49,11 +45,26 @@ prepModName = id
 layoutImport :: ToBriDoc ImportDecl
 layoutImport limportD@(L _ importD) = docWrapNode limportD $ case importD of
   ImportDecl _ (L _ modName) pkg src safe q False as llies -> do
+    importCol <- mAsk <&> _conf_layout .> _lconfig_importColumn .> confUnpack
     let
-      modNameT         = Text.pack $ moduleNameString modName
-      pkgNameT         = Text.pack . prepPkg . sl_st <$> pkg
-
-      asT              = Text.pack . moduleNameString . prepModName <$> as
+      modNameT        = Text.pack $ moduleNameString modName
+      pkgNameT        = Text.pack . prepPkg . sl_st <$> pkg
+      asT             = Text.pack . moduleNameString . prepModName <$> as
+      (hiding, mlies) = case llies of
+        Just (h, L _ lies') -> (h, Just lies')
+        Nothing             -> (False, Nothing)
+      minQLength = length "import qualified "
+      qLengthReal =
+        let qualifiedPart = if q then length "qualified " else 0
+            safePart      = if safe then length "safe " else 0
+            pkgPart       = fromMaybe 0 ((+ 1) . Text.length <$> pkgNameT)
+            srcPart       = if src then length "{-# SOURCE #-} " else 0
+        in  length "import " + srcPart + safePart + qualifiedPart + pkgPart
+      qLength          = max minQLength qLengthReal
+      -- Cost in columns of importColumn
+      asCost           = length "as "
+      bindingCost      = if hiding then length "hiding ( " else length "( "
+      nameCost         = Text.length modNameT + qLength
       importQualifiers = docSeq
         [ appSep $ docLit $ Text.pack "import"
         , if src then appSep $ docLit $ Text.pack "{-# SOURCE #-}" else docEmpty
@@ -61,26 +72,41 @@ layoutImport limportD@(L _ importD) = docWrapNode limportD $ case importD of
         , if q then appSep $ docLit $ Text.pack "qualified" else docEmpty
         , fromMaybe docEmpty (appSep . docLit <$> pkgNameT)
         ]
-      makeAs asT' =
-        appSep $ docSeq [docLit (Text.pack "as"), docSeparator, docLit asT']
-      importIds =
-        docSeq $ [appSep $ docLit modNameT, fromMaybe docEmpty (makeAs <$> asT)]
-    (hiding, ies) <- case llies of
-      Just (h, L _ lies) -> do
-        sies <- docSharedWrapper layoutIEList lies
-        return (h, sies)
-      Nothing -> return (False, docEmpty)
-    h <- docSharedWrapper
-      ( const
-        ( docSeq
-          [ docCols ColImport [importQualifiers, importIds]
-          , if hiding then appSep $ docLit $ Text.pack "hiding" else docEmpty
-          ]
-        )
-      )
-      ()
-    docAlt
-      [ docSeq [h, docForceSingleline ies]
-      , docAddBaseY BrIndentRegular $ docPar h (docForceMultiline ies)
-      ]
+      modNameD =
+        docEnsureIndent (BrIndentSpecial qLength) $ appSep $ docLit modNameT
+      hidDoc =
+        if hiding then appSep $ docLit $ Text.pack "hiding" else docEmpty
+      importHead = docSeq [importQualifiers, modNameD]
+      Just lies  = mlies
+      (ieH:ieT)  = map layoutIE lies
+      makeIENode ie = docSeq [docCommaSep, ie]
+      bindings@(bindingsH:bindingsT) =
+        docSeq [docParenLSep, ieH]
+          :  map makeIENode ieT
+          ++ [docSeq [docSeparator, docParenR]]
+      bindingsD = case mlies of
+        Nothing  -> docSeq [docEmpty]
+        -- ..[hiding].( )
+        Just []  -> docSeq [hidDoc, docParenLSep, docParenR]
+        -- ..[hiding].( b )
+        Just [_] -> docSeq $ hidDoc : bindings
+        -- ..[hiding].( b
+        --            , b'
+        --            )
+        Just _ ->
+          docPar (docSeq [hidDoc, docSetBaseY $ bindingsH]) $ docLines bindingsT
+      bindingLine =
+        docEnsureIndent (BrIndentSpecial (importCol - bindingCost)) bindingsD
+    case asT of
+      Just n | enoughRoom -> docLines [docSeq [importHead, asDoc], bindingLine]
+             | otherwise  -> docLines [importHead, asDoc, bindingLine]
+       where
+        enoughRoom = nameCost < importCol - asCost
+        asDoc =
+          docEnsureIndent (BrIndentSpecial (importCol - asCost))
+            $ docSeq
+            $ [appSep $ docLit $ Text.pack "as", docLit n]
+      Nothing | enoughRoom -> docSeq [importHead, bindingLine]
+              | otherwise  -> docLines [importHead, bindingLine]
+        where enoughRoom = nameCost < importCol - bindingCost
   _ -> docEmpty
